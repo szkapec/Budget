d5b76661afd939aa493b91a119ed088a3598ffaf
szkapec <szkapec999@o2.pl>
2020-05-25 14:04:58 +0200

    Updates

---
+++ precache-manifest.8479cdd44ec7daf2367634e748bd8cf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebea4b01965a6f0a265381fe41a04970",
+    "url": "/budget/index.html"
+  },
+  {
+    "revision": "0ef8efb9f80a98eab4d6",
+    "url": "/budget/static/css/2.14f3cd7d.chunk.css"
+  },
+  {
+    "revision": "d3447cf972f8be44e57c",
+    "url": "/budget/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0ef8efb9f80a98eab4d6",
+    "url": "/budget/static/js/2.bd7168b4.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/budget/static/js/2.bd7168b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3447cf972f8be44e57c",
+    "url": "/budget/static/js/main.06acd11d.chunk.js"
+  },
+  {
+    "revision": "014f7b5065acd0077e74",
+    "url": "/budget/static/js/runtime-main.2b4b19d7.js"
+  }
+]);
